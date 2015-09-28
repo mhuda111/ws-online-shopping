ddcaba7dc97e49e84abef84cf5490ceb2ef04b34
mhuda <mhuda@luc.edu>
2015-09-27 19:40:51 -0500

    billing address update

--- src/main/java/com/project/ws/controller/CustomerBillingDetailsController.java
+++ src/main/java/com/project/ws/controller/CustomerBillingDetailsController.java
@@ -27,4 +27,21 @@ public class CustomerBillingDetailsController {
 		//return customerBillingsRepository.chargeCard(idcustomerId, billId, amount);
     }
 
+	@RequestMapping("/billing/updateAddress")
+    public String updateBillingAddress(HttpServletRequest request) {
+		int customerId = Integer.parseInt(request.getParameter("customerId"));
+		String addrLine1 = request.getParameter("addrLine1");
+		String addrLine2 = request.getParameter("addrLine2");
+		String city = request.getParameter("city");
+		String state = request.getParameter("state");
+		String zip = request.getParameter("zip");
+
+		int updateBill = customerBillingsRepository.updateBillingAddress(customerId, addrLine1, addrLine2,city,state,zip);
+		if (updateBill>0) {
+			return "Successful update billing address";
+		}
+		else return "Denied";
+		//return customerBillingsRepository.chargeCard(idcustomerId, billId, amount);
+    }
+
 }
